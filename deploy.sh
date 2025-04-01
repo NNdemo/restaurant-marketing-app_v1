f4c19b5b73261c222e57633d4ad38c3b61b96d64
qazwsx <qazwsx@qazwsxdeMacBook-Pro.local>
2025-04-01 14:04:29 +0800

    Fix path to point to restaurant-marketing-app_v1

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,31 @@
 # 中止脚本如果发生错误
 set -e
 
+# 清理之前的构建
+rm -rf dist
+
 # 构建
 npm run build
 
 # 进入构建输出目录
 cd dist
 
+# 创建.nojekyll文件阻止GitHub Pages使用Jekyll处理
+touch .nojekyll
+
+# 创建404.html (与index.html相同，用于SPA路由)
+cp index.html 404.html
+
 # 如果需要自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy to restaurant-marketing-app_v1'
 
-# 部署到 https://[USERNAME].github.io/restaurant-marketing-app
-git push -f https://github.com/nndemo/restaurant-marketing-app.git main:gh-pages
+# 部署到 https://[USERNAME].github.io/restaurant-marketing-app_v1
+git push -f https://github.com/NNdemo/restaurant-marketing-app_v1.git main:gh-pages
 
 cd -
+
+echo "部署完成！现在请访问 https://nndemo.github.io/restaurant-marketing-app_v1/"
